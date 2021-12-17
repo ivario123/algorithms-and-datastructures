504b65e7589400039f524f30d6d38f934b28cf8c
Ivar Jönsson <49920136+ivario123@users.noreply.github.com>
2021-12-17 15:32:03 +0100

    bruh

--- datastructures/hash_table/hash_table.py
+++ datastructures/hash_table/hash_table.py
@@ -1,18 +1,36 @@
 class hash_table:
-  def __init__(self,hashing_function,i:int = 0,uses_i = True):
-    self.hash_table = {"":""}
+  def __init__(self,hashing_function,i:int = 0,uses_i = True,table_size:int=1,i_max= 9):
+    self.hash_table = [float("-inf")]*table_size
     self.hashing_function = hashing_function
-    self.uses_i = uses_i
-    self.i = i
-  def hash(self,value):
-    if self.uses_i:
-      self.hash_table[self.hashing_function(value,self.i)] = value
-    else:
-      self.hash_table[self.hashing_function(value)] = value
-    self.i+=1
-    return self.i-1
-  def lookup(self,value,i=0):
-    if self.uses_i:
-      return self.hash_table[self.hashing_function(value,i)]
-    return self.hash_table[self.hashing_function(value)]
+    self.table_size = table_size
+    self.i_max = i_max
+  def hash_using_i(self,value):
+    i = 0
+    hash_key = self.hashing_function(value,i)
+    while self.hash_table[hash_key]!=float('-inf'):
+      i+=1
+      hash_key = self.hashing_function(value,i)
+    self.hash_table[self.hashing_function(value,self.i)] = value
+
+  def hash_using_extend(self,value):
+    hash_key = self.hashing_function(value)
+    if self.hash_table[hash_key] == float('-inf'):
+      self.hash_table[hash_key] = [value]
+      return
+    self.hash_table[hash_key].append(value) 
+  def lookup_using_extend(self,value):
+    hash_key = self.hashing_function(value)
+    if self.hash_table[hash_key] == float('-inf'):
+      return None
+    return hash_key, self.hash_table[hash_key]
+    
+  def lookup_using_i(self,value,i=0):
+    i = 0
+    hash_key = self.hashing_function(value,i)
+    while self.hash_table[hash_key]!=value and i <=self.i_max:
+      i+=1
+      hash_key = self.hashing_function(value,i)
+    if i > self.i_max:
+      return None
+    return hash_key,i
 
